f18b17284faee4e8abba9bc742e3503119d91772
Marko Krizan <markokrizan64@gmail.com>
2019-10-06 23:33:47 +0200

    Fix exception context issue

--- src/app/Exceptions/TokenExpiredException.php
+++ src/app/Exceptions/TokenExpiredException.php
@@ -6,8 +6,9 @@ class TokenExpiredException extends BaseException {
     const STATUS_CODE = 401;
     const MESSAGE = 'Token has expired';
 
-    public function __construct()
+    public function __construct(...$context)
     {
         parent::__construct(self::MESSAGE, self::STATUS_CODE);
+        $this->$context = $context;
     }
 }
--- src/app/Exceptions/UnauthorizedException.php
+++ src/app/Exceptions/UnauthorizedException.php
@@ -6,8 +6,9 @@ class UnauthorizedException extends BaseException {
     const STATUS_CODE = 401;
     const MESSAGE = 'Unauthorized';
 
-    public function __construct()
+    public function __construct(...$context)
     {
         parent::__construct(self::MESSAGE, self::STATUS_CODE);
+        $this->context = $context;
     }
 }
